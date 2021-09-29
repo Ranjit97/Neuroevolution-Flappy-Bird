15dfa5518cc52477a71e3697019aeb92253cdfe9
Ranjit97 <karwal.rojen@gmail.com>
2021-09-30 00:03:05 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,111 @@
+const TOTAL = 500;
+let birds = [];
+let savedBirds = [];
+let pipes = [];
+let counter = 0;
+let cycles = 100;
+let slider;
+var cnv;
+let bird_png;
+let pipe_png;
+let bg;
+
+function keyPressed() {
+    if(key === 'S') {
+        let bird = birds[0];
+    //    let json = bird.brain.serialize();
+        saveJSON(bird.brain,'bird.json');
+    }
+}
+
+function setup() {
+  cnv = createCanvas(600,400);
+  var x = (windowWidth - width) / 2;
+  var y = (windowHeight - height) / 2;
+  cnv.position(x, y);
+  fill(120);
+  bird_png = loadImage('bird.png');
+  pipe_png = loadImage('pipe.png');
+  bg = loadImage('background_png.png');
+  input = createP('Slider to speed up the Training');
+  input.position((windowWidth / 2) - 250,(windowHeight/2) + 210 );
+  //speedup = createElement('h2', 'Slider to spped up the training');
+  slider = createSlider(1, 1000, 1);
+  slider.position((windowWidth / 2) - 250,(windowHeight/2) + 250 );
+  input = createP('(Shift + s) - to save the best agent');
+  input.position((windowWidth / 2) - 250,(windowHeight/2) + 270 );
+  for(let i = 0; i<TOTAL; i++) {
+    birds[i] = new Bird();
+  }
+  //pipes.push(new Pipe());
+}
+
+function draw() {
+  background(bg);
+  var x = (windowWidth - width) / 2;
+  var y = (windowHeight - height) / 2;
+  cnv.position(x, y);
+  
+  for(let n = 0; n<slider.value(); n++) {
+    if(counter % 50 == 0) {
+      pipes.push(new Pipe());
+    }
+    
+    counter++;
+    
+    for(var i = pipes.length-1; i>=0; i--) {
+      //pipes[i].show();
+      pipes[i].update();
+      
+      for(let j = birds.length - 1; j>=0; j--) {
+        if(pipes[i].hits(birds[j])) {
+          savedBirds.push(birds.splice(j, 1)[0]);
+        }
+      }
+      
+      if(pipes[i].offscreen()) {
+        pipes.splice(i,1);
+      }
+    }
+    
+    for(let i = birds.length - 1; i>=0; i--) {
+        if(birds[i].offScreen()) {
+          savedBirds.push(birds.splice(i, 1)[0]);
+        }
+      }
+    
+    for(let bird of birds) {
+      bird.think(pipes);
+      bird.update();
+      //bird.show();
+    }
+    
+    if(birds.length === 0) {
+      counter = 0;
+      nextGeneration();
+      pipes = [];
+    }
+  }
+  
+  //All the drawing Stuff here
+  background(bg);
+    
+  //fill(0);
+  //text( 'Slider to spped up the training'  , (windowWidth / 2) - 250,(windowHeight/2) + 100  );
+  
+  for(let bird of birds) {
+    bird.show();
+  }
+  
+  for(let pipe of pipes) {
+    pipe.show();
+  }
+  
+}
+
+//function keyPressed() {
+//  if(key == ' ') {
+//    bird.up();
+//    //console.log("Space");
+//  }
+//}
